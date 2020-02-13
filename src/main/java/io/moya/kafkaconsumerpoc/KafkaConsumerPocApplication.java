6acc9e7911275e8153cec85b2dad575e44b8919d
= <=>
2020-02-13 11:04:25 -0300

    first commit

---
+++ src/main/java/io/moya/kafkaconsumerpoc/KafkaConsumerPocApplication.java
@@ -0,0 +1,13 @@
+package io.moya.kafkaconsumerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/moya/kafkaconsumerpoc/KafkaConsumerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moya.kafkaconsumerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
